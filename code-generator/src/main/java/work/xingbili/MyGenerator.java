ca578533e61600680d0835304b72fddde7e1509f
xinghuolin <xinghuolin@fusionfintrade.com>
2021-12-16 14:47:26 +0800

    新增代码生成模块

---
+++ code-generator/src/main/java/work/xingbili/MyGenerator.java
@@ -0,0 +1,190 @@
+/**
+ * 版权信息: © 聚均科技
+ */
+
+package work.xingbili;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author xinghuolin
+ * @des: TODO
+ * @date 2021/9/7 9:23
+ */
+public class MyGenerator {
+
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入").append(tip).append("：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotBlank(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    /**
+     * RUN THIS
+     */
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = "D:/";
+        String projectName = "code-generator";
+        gc.setOutputDir(projectPath + "/" + projectName + "/src/main/java");
+        gc.setAuthor("code generator");
+        gc.setOpen(false);
+        gc.setFileOverride(true);
+        gc.setIdType(IdType.ASSIGN_ID);
+
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://129.211.82.202:3306/code_demo?useUnicode=true&serverTimezone=GMT&useSSL=false&characterEncoding=utf8");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("123456");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(scanner("模块名"));
+        pc.setParent("work.xingbili");
+        pc.setService("service.base");
+        pc.setServiceImpl("service.base.impl");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+        // 自定义模板
+        List<FileOutConfig> focList = new ArrayList<>();
+        // entity模板
+        focList.add(new FileOutConfig("/codeTemplates/entity.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return projectPath + "/" + projectName + "/src/main/java/work/xingbili/" + generatModulePath(pc.getModuleName()) + "/entity"
+                        + "/" + tableInfo.getEntityName() + StringPool.DOT_JAVA;
+            }
+        });
+
+        // mapper模板
+        focList.add(new FileOutConfig("/codeTemplates/mapper.java.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return projectPath + "/" + projectName + "/src/main/java/work/xingbili/" + generatModulePath(pc.getModuleName()) + "/mapper"
+                        + "/" + tableInfo.getMapperName() + StringPool.DOT_JAVA;
+            }
+        });
+
+        // mapper.xml模板
+        focList.add(new FileOutConfig("/codeTemplates/mapper.xml.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return projectPath + "/" + projectName + "/src/main/resources/mappers/" + tableInfo.getMapperName() + StringPool.DOT_XML;
+            }
+        });
+
+        // service模板
+        focList.add(new FileOutConfig("/codeTemplates/service.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return projectPath + "/" + projectName + "/src/main/java/work/xingbili/" + generatModulePath(pc.getModuleName()) + "/service/base"
+                        + "/" + tableInfo.getServiceName() + StringPool.DOT_JAVA;
+            }
+        });
+
+        // serviceImpl模板
+        focList.add(new FileOutConfig("/codeTemplates/serviceImpl.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return projectPath + "/" + projectName + "/src/main/java/work/xingbili/" + pc.getModuleName() + "/service/base/impl"
+                        + "/" + tableInfo.getServiceImplName() + StringPool.DOT_JAVA;
+            }
+        });
+
+        // controller模板
+        focList.add(new FileOutConfig("/codeTemplates/controller.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return projectPath + "/" + projectName + "/src/main/java/work/xingbili/" + pc.getModuleName() + "/controller"
+                        + "/" + tableInfo.getControllerName() + StringPool.DOT_JAVA;
+            }
+        });
+
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+        mpg.setTemplate(new TemplateConfig()
+                .setEntity(null)
+                .setMapper(null)
+                .setXml(null)
+                .setService(null)
+                .setServiceImpl(null)
+                .setController(null));
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityTableFieldAnnotationEnable(true);
+        strategy.setVersionFieldName("version");
+        strategy.setTableFillList(Arrays.asList(new TableFill("version", FieldFill.INSERT)));
+        strategy.setRestControllerStyle(true);
+        strategy.setEntityLombokModel(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        // 选择 freemarker 引擎需要指定如下加，注意 pom 依赖必须有！
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+    /**
+     * 获取模板路径
+     *
+     * @param moduleName
+     * @return
+     */
+    private static String generatModulePath(String moduleName) {
+        return org.springframework.util.StringUtils.replace(moduleName, ".", "/");
+    }
+}
+
---
+++ code-generator/target/classes/codeTemplates/controller.ftl
@@ -0,0 +1,43 @@
+<#include "copyright.ftl"/>
+package ${package.Controller};
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+<#if restControllerStyle>
+import org.springframework.web.bind.annotation.RestController;
+<#else>
+import org.springframework.stereotype.Controller;
+</#if>
+<#if superControllerClassPackage??>
+import ${superControllerClassPackage};
+</#if>
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <p>
+ * ${table.comment!} 前端控制器
+ * </p>
+ *
+ * @author ${author}
+ * @since ${date}
+ */
+@Slf4j 
+<#if restControllerStyle>
+@RestController
+<#else>
+@Controller
+</#if>
+@RequestMapping("<#if package.ModuleName?? && package.ModuleName != "">/${package.ModuleName}</#if>/<#if controllerMappingHyphenStyle??>${controllerMappingHyphen}<#else>${table.entityPath}</#if>")
+<#if kotlin>
+class ${table.controllerName}<#if superControllerClass??> : ${superControllerClass}()</#if>
+<#else>
+<#if superControllerClass??>
+public class ${table.controllerName} extends ${superControllerClass} {
+<#else>
+public class ${table.controllerName} {
+</#if>
+
+}
+</#if>
